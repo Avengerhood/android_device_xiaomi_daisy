9e1332b1a9707182e8f2c521c1122232e7029938
Krzysiek <kryzo123456@gmail.com>
2020-06-01 09:55:52 +0200

    New google assistant

--- device.mk
+++ device.mk
@@ -17,8 +17,10 @@
 $(call inherit-product, vendor/xiaomi/daisy/daisy-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, vendor/google/customization/config.mk)
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
+#New Google Assistant
+TARGET_SUPPORTS_NGA := true
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
