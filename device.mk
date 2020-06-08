6391b3e0207aaa0d4c65603932066059336eae9f
Krzysiek <kryzo123456@gmail.com>
2020-06-08 18:53:01 +0200

    Overlay

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 DEVICE_PACKAGE_OVERLAYS += \
      $(LOCAL_PATH)/overlay \
      $(LOCAL_PATH)/overlay-lineage
+     
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+$(LOCAL_PATH)/overlay-lineage/lineage-sdk
   
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
